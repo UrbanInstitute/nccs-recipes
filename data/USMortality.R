207ee488abace44607dd1ef42c6b4d75ed52c1ed
Jesse Lecy <jdlecy@gmail.com>
2023-04-21 15:18:20 -0700

    Add files via upload

---
+++ data/USMortality.R
@@ -0,0 +1,12 @@
+d <-
+structure(list(Status = structure(c(2L, 1L, 2L, 1L, 2L, 1L), .Label = c("Rural", 
+"Urban"), class = "factor"), Sex = structure(c(2L, 2L, 1L, 1L, 
+2L, 2L), .Label = c("Female", "Male"), class = "factor"), Cause = structure(c(6L, 
+6L, 6L, 6L, 2L, 2L), .Label = c("Alzheimers", "Cancer", "Cerebrovascular diseases", 
+"Diabetes", "Flu and pneumonia", "Heart disease", "Lower respiratory", 
+"Nephritis", "Suicide", "Unintentional injuries"), class = "factor"), 
+    Rate = c(210.19999999999999, 242.69999999999999, 132.5, 154.90000000000001, 
+    195.90000000000001, 219.30000000000001), SE = c(0.20000000000000001, 
+    0.59999999999999998, 0.20000000000000001, 0.40000000000000002, 
+    0.20000000000000001, 0.5)), row.names = c(1L, 2L, 3L, 4L, 
+53L, 54L), class = "data.frame")
